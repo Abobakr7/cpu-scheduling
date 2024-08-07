7d570b29c0e7ede228c4dbd14b60eea28109b2b1
Abobakr <mabobakr79@yahoo.com>
2024-08-07 20:03:51 +0300

    finished project

---
+++ src/main/java/app/cpu/scheduling/CpuScheduling.java
@@ -0,0 +1,10 @@
+package app.cpu.scheduling;
+
+public class CpuScheduling {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
